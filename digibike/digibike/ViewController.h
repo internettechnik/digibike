67201934a19e689a03c4074082558bacf2e57a81
John <john@KPersM6160.local>
2013-02-02 17:27:05 +0100

    we move AppDelegate to supporting files (not so important any more...)

--- digibike/digibike/ViewController.h
+++ digibike/digibike/ViewController.h
@@ -19,6 +19,9 @@
 //       with this Controller-element "statusText":
 //       by CTRL-dragging from View-Controller to the label,
 //       thereby select "statusText"...
+// weak, because if view "goes away" the statusText MUST NOT
+//       be removed from memory (so we not use "strong")
+// nonatomic => not thread safe
 @property (weak, nonatomic) IBOutlet UILabel *statusText;
 
 @end
